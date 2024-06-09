06581730a70222bf8f564a2ce72dfa5e15a39c5e
Fischsalat <64608145+Fischsalat@users.noreply.github.com>
2024-06-09 17:28:17 +0200

    Improved FindLevelActorsOffset().

--- Dumper/OffsetFinder.h
+++ Dumper/OffsetFinder.h
@@ -629,6 +629,7 @@ namespace OffsetFinder
 	/* ULevel */
 	inline int32_t FindLevelActorsOffset()
 	{
+		UEObject Level = nullptr;
 		uintptr_t Lvl = 0x0;
 
 		for (auto Obj : ObjectArray())
@@ -636,6 +637,7 @@ namespace OffsetFinder
 			if (Obj.HasAnyFlags(EObjectFlags::ClassDefaultObject) || !Obj.IsA(EClassCastFlags::Level))
 				continue;
 
+			Level = Obj;
 			Lvl = reinterpret_cast<uintptr_t>(Obj.GetAddress());
 			break;
 		}
@@ -655,13 +657,8 @@ namespace OffsetFinder
 		SearchEnd = offsetof(ULevel, OwningWorld)
 		*/
 
-		UEClass LevelClass = ObjectArray::FindClassFast("Level");
-
-		if (!LevelClass) [[unlikely]]
-			LevelClass = ObjectArray::FindClassFast("level");
-
 		int32 SearchStart = ObjectArray::FindClassFast("Object").GetStructSize() + ObjectArray::FindObjectFast<UEStruct>("URL", EClassCastFlags::Struct).GetStructSize();
-		int32 SearchEnd = LevelClass.FindMember("OwningWorld").GetOffset();
+		int32 SearchEnd = Level.GetClass().FindMember("OwningWorld").GetOffset();
 
 		for (int i = SearchStart; i <= (SearchEnd - 0x10); i += 8)
 		{
