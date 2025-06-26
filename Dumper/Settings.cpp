907d1b151279320bde0995b2aa97dcff85f38ff3
lmachens <leon@machens.koeln>
2025-06-26 17:29:53 +0200

    Format

--- Dumper/Settings.cpp
+++ Dumper/Settings.cpp
@@ -8,25 +8,28 @@ void Settings::Config::Load()
     namespace fs = std::filesystem;
 
     // Try local Dumper-7.ini
-    std::string localPath = (fs::current_path() / "Dumper-7.ini").string();
-    const char* configPath = nullptr;
+    std::string LocalPath = (fs::current_path() / "Dumper-7.ini").string();
+    const char* ConfigPath = nullptr;
 
-    if (fs::exists(localPath)) {
-        configPath = localPath.c_str();
+    if (fs::exists(LocalPath)) 
+    {
+        ConfigPath = LocalPath.c_str();
     }
     // Try global path
-    else if (fs::exists("C:/Dumper-7/Dumper-7.ini")) {
-        configPath = "C:/Dumper-7/Dumper-7.ini";
+    else if (fs::exists("C:/Dumper-7/Dumper-7.ini")) 
+    {
+        ConfigPath = "C:/Dumper-7/Dumper-7.ini";
     }
 
     // If no config found, use defaults
-    if (!configPath) {
+    if (!ConfigPath) 
+    {
         return;
     }
 
-    char sdkNamespace[256] = {};
-    GetPrivateProfileStringA("Settings", "SDKNamespaceName", "SDK", sdkNamespace, sizeof(sdkNamespace), configPath);
-    SDKNamespaceName = sdkNamespace;
+    char SDKNamespace[256] = {};
+    GetPrivateProfileStringA("Settings", "SDKNamespaceName", "SDK", SDKNamespace, sizeof(SDKNamespace), ConfigPath);
+    SDKNamespaceName = SDKNamespace;
 
-    SleepTimeout = GetPrivateProfileIntA("Settings", "SleepTimeout", 0, configPath);
+    SleepTimeout = GetPrivateProfileIntA("Settings", "SleepTimeout", 0, ConfigPath);
 }
