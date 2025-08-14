b31525b69466f9a3d822d110993ba7d0f060553b
igromanru <igromanru@yahoo.de>
2025-08-14 14:05:21 +0200

    feat: Add `CppGenerator` setting `bAddFinalSpecifier` (enabled by default) to control whether the `final` specifier is generated for classes marked as final.

--- Dumper/Generator/Private/Generators/CppGenerator.cpp
+++ Dumper/Generator/Private/Generators/CppGenerator.cpp
@@ -684,7 +684,7 @@ void CppGenerator::GenerateStruct(const StructWrapper& Struct, StreamType& Struc
   , bIsClass ? "class" : (bIsUnion ? "union" : "struct")
   , Struct.ShouldUseExplicitAlignment() || bHasReusedTrailingPadding ? std::format("alignas(0x{:02X}) ", Struct.GetAlignment()) : ""
   , UniqueName
-  , Struct.IsFinal() ? " final" : ""
+  , Settings::CppGenerator::bAddFinalSpecifier && Struct.IsFinal() ? " final" : ""
   , bHasValidSuper ? (" : public " + UniqueSuperName) : "");
 
 	MemberManager Members = Struct.GetMembers();
