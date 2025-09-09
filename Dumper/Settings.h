b4263cb404448578e60835e46b1fe5a795ed2ba6
lobotomyx <lbatvt@gmail.com>
2025-09-09 18:13:11 -0400

    Add new settings and example config
    In addition to sleep timeout we can now set a path for the output SDK and specify a key to trigger dumps with handling to allow both a key and a timeout option. Configs can now also be placed next to the DLL (placing next to exe will have highest priority)
    Added an example config and markdown file to explain changes to the masses

--- Dumper/Settings.h
+++ Dumper/Settings.h
@@ -6,160 +6,167 @@
 
 namespace Settings
 {
-	constexpr bool Is32Bit()
-	{
+    constexpr bool Is32Bit()
+    {
 #if defined(_WIN64)
-		return false;
+        return false;
 #elif defined(_WIN32)
-		return true;
+        return true;
 #endif
-	}
+    }
   
-	inline constexpr const char* GlobalConfigPath = "C:/Dumper-7/Dumper-7.ini";
+    inline constexpr const char* GlobalConfigPath = "C:/Dumper-7/Dumper-7.ini";
 
-	namespace Config
-	{
-		inline int SleepTimeout = 0;
-		inline std::string SDKNamespaceName = "SDK";
+    namespace Config
+    {
+        /* 
+        this doesn't really need a default value since it will be set in dllmain while we have our own handle
+        this is actually just so we can check for a dll-adjacent config file without having to call GetModuleHandle("Dumper-7.dll")
+        which I chose not to do mainly because I also added a proxy loader in my fork. I could also push that over 
+        but I kind of don't want to make it that easy for people
+         */
+        inline std::string ModulePath = ".\\Dumper-7.dll";
+        inline int SleepTimeout = 0;
+        inline int DumpKey = 0;
+        inline std::string SDKNamespaceName = "SDK";
+        void Load();
+    };
 
-		void Load();
-	};
+    namespace EngineCore
+    {
+        /* A special setting to fix UEnum::Names where the type is sometimes TArray<FName> and sometimes TArray<TPair<FName, Some8BitData>> */
+        constexpr bool bCheckEnumNamesInUEnum = true;
 
-	namespace EngineCore
-	{
-		/* A special setting to fix UEnum::Names where the type is sometimes TArray<FName> and sometimes TArray<TPair<FName, Some8BitData>> */
-		constexpr bool bCheckEnumNamesInUEnum = true;
+        /* Enables support for TEncryptedObjectProperty */
+        constexpr bool bEnableEncryptedObjectPropertySupport = false;
+    }
 
-		/* Enables support for TEncryptedObjectProperty */
-		constexpr bool bEnableEncryptedObjectPropertySupport = false;
-	}
+    namespace Generator
+    {
+        //Auto generated if no override is provided
+        inline std::string GameName = "";
+        inline std::string GameVersion = "";
+        // kind of awkward not to have it in the config namespace but whatever, its fine like this I think
+        inline std::string SDKGenerationPath = "C:/Dumper-7";
+    }
 
-	namespace Generator
-	{
-		//Auto generated if no override is provided
-		inline std::string GameName = "";
-		inline std::string GameVersion = "";
+    namespace CppGenerator
+    {
+        /* No prefix for files->FilePrefix = "" */
+        constexpr const char* FilePrefix = "";
 
-		inline constexpr const char* SDKGenerationPath = "C:/Dumper-7";
-	}
+        /* No seperate namespace for Params -> ParamNamespaceName = nullptr */
+        constexpr const char* ParamNamespaceName = "Params";
 
-	namespace CppGenerator
-	{
-		/* No prefix for files->FilePrefix = "" */
-		constexpr const char* FilePrefix = "";
+        /* XOR function name, that will be wrapped around any generated string. e.g. "xorstr_" -> xorstr_("Pawn") etc. */
+        constexpr const char* XORString = nullptr;
+        /* XOR header file name. e.g. "xorstr.hpp" */
+        constexpr const char* XORStringInclude = nullptr;
 
-		/* No seperate namespace for Params -> ParamNamespaceName = nullptr */
-		constexpr const char* ParamNamespaceName = "Params";
-
-		/* XOR function name, that will be wrapped around any generated string. e.g. "xorstr_" -> xorstr_("Pawn") etc. */
-		constexpr const char* XORString = nullptr;
-		/* XOR header file name. e.g. "xorstr.hpp" */
-		constexpr const char* XORStringInclude = nullptr;
-
-		/* Customizable part of Cpp code to allow for a custom 'uintptr_t InSDKUtils::GetImageBase()' function */
-		constexpr const char* GetImageBaseFuncBody = 
+        /* Customizable part of Cpp code to allow for a custom 'uintptr_t InSDKUtils::GetImageBase()' function */
+        constexpr const char* GetImageBaseFuncBody = 
 R"({
-	return reinterpret_cast<uintptr_t>(GetModuleHandle(0));
+    return reinterpret_cast<uintptr_t>(GetModuleHandle(0));
 }
 )";
-		/* Customizable part of Cpp code to allow for a custom 'InSDKUtils::CallGameFunction' function */
-		constexpr const char* CallGameFunction =
+        /* Customizable part of Cpp code to allow for a custom 'InSDKUtils::CallGameFunction' function */
+        constexpr const char* CallGameFunction =
 R"(
-	template<typename FuncType, typename... ParamTypes>
-	requires std::invocable<FuncType, ParamTypes...>
-	inline auto CallGameFunction(FuncType Function, ParamTypes&&... Args)
-	{
-		return Function(std::forward<ParamTypes>(Args)...);
-	}
+    template<typename FuncType, typename... ParamTypes>
+    requires std::invocable<FuncType, ParamTypes...>
+    inline auto CallGameFunction(FuncType Function, ParamTypes&&... Args)
+    {
+        return Function(std::forward<ParamTypes>(Args)...);
+    }
 )";
-		/* An option to force the UWorld::GetWorld() function in the SDK to get the world through an instance of UEngine. Useful for games on which the dumper finds the wrong GWorld offset. */
-		constexpr bool bForceNoGWorldInSDK = false;
+        /* An option to force the UWorld::GetWorld() function in the SDK to get the world through an instance of UEngine. Useful for games on which the dumper finds the wrong GWorld offset. */
+        constexpr bool bForceNoGWorldInSDK = false;
 
-		/* This will allow the user to manually initialize global variable addresses in the SDK (eg. GObjects, GNames, AppendString). */
-		constexpr bool bAddManualOverrideOptions = true;
+        /* This will allow the user to manually initialize global variable addresses in the SDK (eg. GObjects, GNames, AppendString). */
+        constexpr bool bAddManualOverrideOptions = true;
 
-		/* Adds the 'final' specifier to classes with no loaded child class at SDK-generation time. */
-		constexpr bool bAddFinalSpecifier = true;
-	}
+        /* Adds the 'final' specifier to classes with no loaded child class at SDK-generation time. */
+        constexpr bool bAddFinalSpecifier = true;
+    }
 
-	namespace MappingGenerator
-	{
-		/* Whether the MappingGenerator should check if a name was written to the nametable before. Exists to reduce mapping size. */
-		constexpr bool bShouldCheckForDuplicatedNames = true;
+    namespace MappingGenerator
+    {
+        /* Whether the MappingGenerator should check if a name was written to the nametable before. Exists to reduce mapping size. */
+        constexpr bool bShouldCheckForDuplicatedNames = true;
 
-		/* Whether EditorOnly should be excluded from the mapping file. */
-		constexpr bool bExcludeEditorOnlyProperties = true;
+        /* Whether EditorOnly should be excluded from the mapping file. */
+        constexpr bool bExcludeEditorOnlyProperties = true;
 
-		/* Which compression method to use when generating the file. */
-		constexpr EUsmapCompressionMethod CompressionMethod = EUsmapCompressionMethod::ZStandard;
-	}
+        /* Which compression method to use when generating the file. */
+        constexpr EUsmapCompressionMethod CompressionMethod = EUsmapCompressionMethod::ZStandard;
+    }
 
-	/* Partially implemented  */
-	namespace Debug
-	{
-		/* Generates a dedicated file defining macros for static asserts (Make sure InlineAssertions are off) */
-		inline constexpr bool bGenerateAssertionFile = true;
+    /* Partially implemented  */
+    namespace Debug
+    {
+        /* Generates a dedicated file defining macros for static asserts (Make sure InlineAssertions are off) */
+        inline constexpr bool bGenerateAssertionFile = true;
 
-		/* Prefix for assertion macros in assertion file. Example for "MyPackage_params.hpp": #define DUMPER7_ASSERTS_PARAMS_MyPackage */
-		inline constexpr const char* AssertionMacroPrefix = "DUMPER7_ASSERTS_";
-
-
-		/* Adds static_assert for struct-size, as well as struct-alignment */
-		inline constexpr bool bGenerateInlineAssertionsForStructSize = false;
-
-		/* Adds static_assert for member-offsets */
-		inline constexpr bool bGenerateInlineAssertionsForStructMembers = false;
+        /* Prefix for assertion macros in assertion file. Example for "MyPackage_params.hpp": #define DUMPER7_ASSERTS_PARAMS_MyPackage */
+        inline constexpr const char* AssertionMacroPrefix = "DUMPER7_ASSERTS_";
 
 
-		/* Prints debug information during Mapping-Generation */
-		inline constexpr bool bShouldPrintMappingDebugData = false;
-	}
+        /* Adds static_assert for struct-size, as well as struct-alignment */
+        inline constexpr bool bGenerateInlineAssertionsForStructSize = false;
 
-	//* * * * * * * * * * * * * * * * * * * * *// 
-	// Do **NOT** change any of these settings //
-	//* * * * * * * * * * * * * * * * * * * * *//
-	namespace Internal
-	{
-		/* Whether UEnum::Names stores only the name of the enum value, or a Pair<Name, Value> */
-		inline bool bIsEnumNameOnly = false; // EDemoPlayFailure
+        /* Adds static_assert for member-offsets */
+        inline constexpr bool bGenerateInlineAssertionsForStructMembers = false;
 
-		/* Whether the 'Value' component in the Pair<Name, Value> UEnum::Names is a uint8 value, rather than the default int64 */
-		inline bool bIsSmallEnumValue = false;
 
-		/* Whether TWeakObjectPtr contains 'TagAtLastTest' */
-		inline bool bIsWeakObjectPtrWithoutTag = false;
+        /* Prints debug information during Mapping-Generation */
+        inline constexpr bool bShouldPrintMappingDebugData = false;
+    }
 
-		/* Whether this games' engine version uses FProperty rather than UProperty */
-		inline bool bUseFProperty = false;
+    //* * * * * * * * * * * * * * * * * * * * *// 
+    // Do **NOT** change any of these settings //
+    //* * * * * * * * * * * * * * * * * * * * *//
+    namespace Internal
+    {
+        /* Whether UEnum::Names stores only the name of the enum value, or a Pair<Name, Value> */
+        inline bool bIsEnumNameOnly = false; // EDemoPlayFailure
 
-		/* Whether this game's engine version uses FNamePool rather than TNameEntryArray */
-		inline bool bUseNamePool = false;
+        /* Whether the 'Value' component in the Pair<Name, Value> UEnum::Names is a uint8 value, rather than the default int64 */
+        inline bool bIsSmallEnumValue = false;
 
-		/* Whether UObject::Name or UObject::Class is first. Affects the calculation of the size of FName in fixup code. Not used after Off::Init(); */
-		inline bool bIsObjectNameBeforeClass = false;
+        /* Whether TWeakObjectPtr contains 'TagAtLastTest' */
+        inline bool bIsWeakObjectPtrWithoutTag = false;
 
-		/* Whether this games uses case-sensitive FNames, adding int32 DisplayIndex to FName */
-		inline bool bUseCasePreservingName = false;
+        /* Whether this games' engine version uses FProperty rather than UProperty */
+        inline bool bUseFProperty = false;
 
-		/* Whether this games uses FNameOutlineNumber, moving the 'Number' component from FName into FNameEntry inside of FNamePool */
-		inline bool bUseOutlineNumberName = false;
+        /* Whether this game's engine version uses FNamePool rather than TNameEntryArray */
+        inline bool bUseNamePool = false;
 
-		/* Whether this game uses the 'FFieldPathProperty' cast flags for a custom property 'FObjectPtrProperty' */
-		inline bool bIsObjPtrInsteadOfFieldPathProperty = false;
+        /* Whether UObject::Name or UObject::Class is first. Affects the calculation of the size of FName in fixup code. Not used after Off::Init(); */
+        inline bool bIsObjectNameBeforeClass = false;
 
-		/* Whether this games' engine version uses a contexpr flag to determine whether a FFieldVariant holds a UObject* or FField* */
-		inline bool bUseMaskForFieldOwner = false;
+        /* Whether this games uses case-sensitive FNames, adding int32 DisplayIndex to FName */
+        inline bool bUseCasePreservingName = false;
 
-		/* Whether this games' engine version uses double for FVector, instead of float. Aka, whether the engine version is UE5.0 or higher. */
-		inline bool bUseLargeWorldCoordinates = false;
+        /* Whether this games uses FNameOutlineNumber, moving the 'Number' component from FName into FNameEntry inside of FNamePool */
+        inline bool bUseOutlineNumberName = false;
 
-		/* Whether this game uses uint8 for UEProperty::ArrayDim, instead of int32 */
-		inline bool bUseUint8ArrayDim = false;
-	}
+        /* Whether this game uses the 'FFieldPathProperty' cast flags for a custom property 'FObjectPtrProperty' */
+        inline bool bIsObjPtrInsteadOfFieldPathProperty = false;
 
-	extern void InitWeakObjectPtrSettings();
-	extern void InitLargeWorldCoordinateSettings();
+        /* Whether this games' engine version uses a contexpr flag to determine whether a FFieldVariant holds a UObject* or FField* */
+        inline bool bUseMaskForFieldOwner = false;
 
-	extern void InitObjectPtrPropertySettings();
-	extern void InitArrayDimSizeSettings();
+        /* Whether this games' engine version uses double for FVector, instead of float. Aka, whether the engine version is UE5.0 or higher. */
+        inline bool bUseLargeWorldCoordinates = false;
+
+        /* Whether this game uses uint8 for UEProperty::ArrayDim, instead of int32 */
+        inline bool bUseUint8ArrayDim = false;
+    }
+
+    extern void InitWeakObjectPtrSettings();
+    extern void InitLargeWorldCoordinateSettings();
+
+    extern void InitObjectPtrPropertySettings();
+    extern void InitArrayDimSizeSettings();
 }
--- Dumper/main.cpp
+++ Dumper/main.cpp
@@ -1,7 +1,10 @@
-#include <Windows.h>
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN   
 #include <chrono>
-#include <fstream>
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <windows.h>
 
 #include "Generators/CppGenerator.h"
 #include "Generators/MappingGenerator.h"
@@ -9,6 +12,27 @@
 #include "Generators/DumpspaceGenerator.h"
 
 #include "Generators/Generator.h"
+
+// kind of doesnt need to be its own function but doesnt hurt either
+std::filesystem::path get_module_path(HMODULE module)
+{
+    WCHAR buf[256];
+    return GetModuleFileNameW(module, buf, ARRAYSIZE(buf)) ? buf : std::filesystem::path();
+}
+
+// Using atomics to allow waiting for keypress in a separate thread without race conditions
+std::atomic<bool> keyPressed = false;
+
+// Reads ints from the ini file, use the actual value for VK scancodes, e.g. 0x77 = F8
+void listener(int key)
+{
+    while (!keyPressed)
+    {
+        keyPressed = GetAsyncKeyState(key) & 0x8000;
+    }
+    Sleep(50);
+}
+
 
 enum class EFortToastType : uint8
 {
@@ -18,84 +42,114 @@ enum class EFortToastType : uint8
         EFortToastType_MAX             = 3,
 };
 
-DWORD MainThread(HMODULE Module)
-{
-	AllocConsole();
-	FILE* Dummy;
-	freopen_s(&Dummy, "CONOUT$", "w", stderr);
-	freopen_s(&Dummy, "CONIN$", "r", stdin);
 
-	auto t_1 = std::chrono::high_resolution_clock::now();
+// Kind of had to make this a separate function for the control flow
+static int Dump(HMODULE Module) {
+    AllocConsole();
+    FILE* Dummy;
+    freopen_s(&Dummy, "CONOUT$", "w", stderr);
+    freopen_s(&Dummy, "CONIN$", "r", stdin);
 
-	std::cerr << "Started Generation [Dumper-7]!\n";
+    auto t_1 = std::chrono::high_resolution_clock::now();
 
-	Settings::Config::Load();
+    std::cerr << "Started Generation [Dumper-7]!\n";
+    Generator::InitEngineCore();
+    Generator::InitInternal();
 
-	if (Settings::Config::SleepTimeout > 0)
-	{
-		std::cerr << "Sleeping for " << Settings::Config::SleepTimeout << "ms...\n";
-		Sleep(Settings::Config::SleepTimeout);
-	}
+    if (Settings::Generator::GameName.empty() && Settings::Generator::GameVersion.empty())
+    {
+        // Only Possible in Main()
+        FString Name;
+        FString Version;
+        UEClass Kismet = ObjectArray::FindClassFast("KismetSystemLibrary");
+        UEFunction GetGameName = Kismet.GetFunction("KismetSystemLibrary", "GetGameName");
+        UEFunction GetEngineVersion = Kismet.GetFunction("KismetSystemLibrary", "GetEngineVersion");
 
-	Generator::InitEngineCore();
-	Generator::InitInternal();
+        Kismet.ProcessEvent(GetGameName, &Name);
+        Kismet.ProcessEvent(GetEngineVersion, &Version);
 
-	if (Settings::Generator::GameName.empty() && Settings::Generator::GameVersion.empty())
-	{
-		// Only Possible in Main()
-		FString Name;
-		FString Version;
-		UEClass Kismet = ObjectArray::FindClassFast("KismetSystemLibrary");
-		UEFunction GetGameName = Kismet.GetFunction("KismetSystemLibrary", "GetGameName");
-		UEFunction GetEngineVersion = Kismet.GetFunction("KismetSystemLibrary", "GetEngineVersion");
+        Settings::Generator::GameName = Name.ToString();
+        Settings::Generator::GameVersion = Version.ToString();
+        Settings::Internal::bUseLargeWorldCoordinates = Version.ToString().find("UE4") != std::string::npos ? false : true;
+    }
 
-		Kismet.ProcessEvent(GetGameName, &Name);
-		Kismet.ProcessEvent(GetEngineVersion, &Version);
+    std::cerr << "GameName: " << Settings::Generator::GameName << "\n";
+    std::cerr << "GameVersion: " << Settings::Generator::GameVersion << "\n\n";
+    Generator::Generate<MappingGenerator>();
+    Generator::Generate<CppGenerator>();
+    Generator::Generate<IDAMappingGenerator>();
+    Generator::Generate<DumpspaceGenerator>();
 
-		Settings::Generator::GameName = Name.ToString();
-		Settings::Generator::GameVersion = Version.ToString();
-	}
+    auto t_C = std::chrono::high_resolution_clock::now();
 
-	std::cerr << "GameName: " << Settings::Generator::GameName << "\n";
-	std::cerr << "GameVersion: " << Settings::Generator::GameVersion << "\n\n";
+    auto ms_int_ = std::chrono::duration_cast<std::chrono::milliseconds>(t_C - t_1);
+    std::chrono::duration<double, std::milli> ms_double_ = t_C - t_1;
 
-	Generator::Generate<CppGenerator>();
-	Generator::Generate<MappingGenerator>();
-	Generator::Generate<IDAMappingGenerator>();
-	Generator::Generate<DumpspaceGenerator>();
+    std::cerr << "\n\nGenerating SDK took (" << ms_double_.count() << "ms)\n\n\n";
+    
+    // wait a few seconds and then unload automatically
+    // If someone wants to prevent this they can just click on the console output to pause execution
+    // actually this change doesn't matter, but I felt like it was odd to have an exit key and not have it configurable
+    // maybe just reuse the dumpkey if its given or just revert this portion idc
+    Sleep(5000);
+    fclose(stderr);
+    if (Dummy) fclose(Dummy);
+    FreeConsole();
 
-	auto t_C = std::chrono::high_resolution_clock::now();
-
-	auto ms_int_ = std::chrono::duration_cast<std::chrono::milliseconds>(t_C - t_1);
-	std::chrono::duration<double, std::milli> ms_double_ = t_C - t_1;
-
-	std::cerr << "\n\nGenerating SDK took (" << ms_double_.count() << "ms)\n\n\n";
-
-	while (true)
-	{
-		if (GetAsyncKeyState(VK_F6) & 1)
-		{
-			fclose(stderr);
-			if (Dummy) fclose(Dummy);
-			FreeConsole();
-
-			FreeLibraryAndExitThread(Module, 0);
-		}
-
-		Sleep(100);
-	}
-
-	return 0;
+    FreeLibraryAndExitThread(Module, 0);
+    return 0;
 }
+
+
+
+DWORD MainThread(HMODULE Module) {
+    // Grab the path to the dll in case you want to store your settings file adjacent to it and have it out of the game directory
+    Settings::Config::ModulePath = get_module_path(Module).string();
+    Settings::Config::Load();
+    std::cerr << Settings::Generator::SDKGenerationPath;
+    static auto t_0 = std::chrono::high_resolution_clock::now();
+    // Dump immediately if no timeout or dump key is set
+    if (Settings::Config::SleepTimeout + Settings::Config::DumpKey == 0)
+        return Dump(Module);
+    // If a timeout is set without a dumpkey we will just synchronously wait and then dump
+    if (Settings::Config::DumpKey == 0) {
+        Sleep(Settings::Config::SleepTimeout);
+        return Dump(Module);
+    }
+    // When a dump key is set we need to suspend execution and wait asynchronously to avoid unloading or dumping early
+    else {
+        std::thread listenerThread(&listener, Settings::Config::DumpKey);
+        listenerThread.detach();
+        auto t_x = std::chrono::high_resolution_clock::now();
+        while (!keyPressed)
+        {
+        // And finally in the case that both a dump key and timeout are set we will continue checking back on this thread
+            if (Settings::Config::SleepTimeout > 0) {
+                t_x = std::chrono::high_resolution_clock::now();
+                auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t_x - t_0).count();
+                if (diff >= Settings::Config::SleepTimeout) {
+                    std::cerr << "Timeout reached, dumping SDK...\n";
+                    return Dump(Module);
+                }
+            }
+            Sleep(100);
+        }
+        return Dump(Module);
+    }
+
+}
+
+
+
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-	switch (reason)
-	{
-	case DLL_PROCESS_ATTACH:
-		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
-		break;
-	}
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
+        break;
+    }
 
-	return TRUE;
+    return TRUE;
 }
