e574249a7a77fdb007978b6cc68309b7dc20c55b
Evasion3356 <Evasion3356@proton.me>
2026-03-16 22:16:37 -0400

    fix: restore indentation in main.cpp

--- Dumper/main.cpp
+++ Dumper/main.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h>
+﻿#include <Windows.h>
 #include <iostream>
 #include <chrono>
 
@@ -7,90 +7,82 @@
 
 DWORD MainThread(HMODULE Module)
 {
-AllocConsole();
-FILE* Dummy;
-freopen_s(&Dummy, "CONOUT$", "w", stderr);
-freopen_s(&Dummy, "CONIN$", "r", stdin);
+	AllocConsole();
+	FILE* Dummy;
+	freopen_s(&Dummy, "CONOUT$", "w", stderr);
+	freopen_s(&Dummy, "CONIN$", "r", stdin);
 
-std::cerr << "Started Generation [Dumper-7]!\n";
+	std::cerr << "Started Generation [Dumper-7]!\n";
 
-Settings::Config::Load();
+	Settings::Config::Load();
 
-if (Settings::Config::SleepTimeout > 0)
-{
-std::cerr << "Sleeping for " << Settings::Config::SleepTimeout << "ms...\n";
-Sleep(Settings::Config::SleepTimeout);
-}
+	if (Settings::Config::SleepTimeout > 0)
+	{
+		std::cerr << "Sleeping for " << Settings::Config::SleepTimeout << "ms...\n";
+		Sleep(Settings::Config::SleepTimeout);
+	}
 
-Generator::InitEngineCore();
+	Generator::InitEngineCore();
 
-auto Eject = [&]()
-{
-fclose(stderr);
-if (Dummy) fclose(Dummy);
-FreeConsole();
-FreeLibraryAndExitThread(Module, 0);
-};
+	auto Eject = [&]()
+	{
+		fclose(stderr);
+		if (Dummy) fclose(Dummy);
+		FreeConsole();
+		FreeLibraryAndExitThread(Module, 0);
+	};
 
-if (Settings::Config::bMultiLevelCapture)
-{
-std::cerr << "\n[Dumper-7] Multi-level capture mode:\n";
-std::cerr << "  F4 = Capture current classes (press in each level)\n";
-std::cerr << "  F5 = Generate SDK from all captured classes\n";
-std::cerr << "  F6 = Eject\n\n";
+	if (Settings::Config::bMultiLevelCapture)
+	{
+		std::cerr << "\n[Dumper-7] Multi-level capture mode:\n";
+		std::cerr << "  F4 = Capture current classes (press in each level)\n";
+		std::cerr << "  F5 = Generate SDK from all captured classes\n";
+		std::cerr << "  F6 = Eject\n\n";
 
-while (true)
-{
-if (GetAsyncKeyState(VK_F4) & 1)
-MultiLevelCapture::CaptureCurrentClasses();
+		while (true)
+		{
+			if (GetAsyncKeyState(VK_F4) & 1)
+				MultiLevelCapture::CaptureCurrentClasses();
 
-if (GetAsyncKeyState(VK_F5) & 1)
-MultiLevelCapture::GenerateSDK();
+			if (GetAsyncKeyState(VK_F5) & 1)
+				MultiLevelCapture::GenerateSDK();
 
-if (GetAsyncKeyState(VK_F6) & 1)
-Eject();
+			if (GetAsyncKeyState(VK_F6) & 1)
+				Eject();
 
-Sleep(100);
-}
-}
-else
-{
-std::cerr << "\n[Dumper-7] Controls:\n";
-std::cerr << "  F5 = Generate SDK\n";
-std::cerr << "  F6 = Eject\n\n";
+			Sleep(100);
+		}
+	}
+	else
+	{
+		const auto StartTime = std::chrono::high_resolution_clock::now();
 
-while (true)
-{
-if (GetAsyncKeyState(VK_F5) & 1)
-{
-const auto StartTime = std::chrono::high_resolution_clock::now();
+		MultiLevelCapture::RunGenerators();
 
-MultiLevelCapture::RunGenerators();
+		const auto EndTime = std::chrono::high_resolution_clock::now();
+		const std::chrono::duration<double, std::milli> DumpTime = EndTime - StartTime;
+		std::cerr << std::format("\n\nGenerating SDK took ({:.0f}ms)\n\n\n", DumpTime.count());
 
-const auto EndTime = std::chrono::high_resolution_clock::now();
-const std::chrono::duration<double, std::milli> DumpTime = EndTime - StartTime;
-std::cerr << std::format("\n\nGenerating SDK took ({:.0f}ms)\n\n\n", DumpTime.count());
-}
+		while (true)
+		{
+			if (GetAsyncKeyState(VK_F6) & 1)
+				Eject();
 
-if (GetAsyncKeyState(VK_F6) & 1)
-Eject();
+			Sleep(100);
+		}
+	}
 
-Sleep(100);
-}
-}
-
-return 0;
+	return 0;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
-switch (reason)
-{
-case DLL_PROCESS_ATTACH:
-CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
-break;
-}
+	switch (reason)
+	{
+	case DLL_PROCESS_ATTACH:
+		CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0);
+		break;
+	}
 
-return TRUE;
+	return TRUE;
 }
-
