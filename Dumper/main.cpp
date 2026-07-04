1ba0a1f3bcaa8b102f014f871cd7720505a79c67
Fischsalat <64608145+Fischsalat@users.noreply.github.com>
2026-07-04 21:01:49 +0200

    Fixed indent on EFortToastType

--- Dumper/main.cpp
+++ Dumper/main.cpp
@@ -12,10 +12,10 @@
 
 enum class EFortToastType : uint8
 {
-        Default                        = 0,
-        Subdued                        = 1,
-        Impactful                      = 2,
-        EFortToastType_MAX             = 3,
+	Default                        = 0,
+	Subdued                        = 1,
+	Impactful                      = 2,
+	EFortToastType_MAX             = 3,
 };
 
 DWORD MainThread(HMODULE Module)
