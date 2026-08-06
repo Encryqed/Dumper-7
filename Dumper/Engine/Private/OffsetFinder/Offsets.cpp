5da2c806f3b6368315a8f57a1dcf442b774ade3f
alexbowe <alex@alexbowe.com>
2026-08-06 15:49:28 -0700

    refactor: derive FFieldClass spacing from field types

--- Dumper/Engine/Private/OffsetFinder/Offsets.cpp
+++ Dumper/Engine/Private/OffsetFinder/Offsets.cpp
@@ -348,10 +348,11 @@ void Off::Init()
 		Off::FFieldClass::CastFlags = OffsetFinder::FindFieldClassCastFlagsOffset();
 
 		/* UE5.7 moved ClassFlags before Id. */
-		if (Off::FFieldClass::CastFlags == (Off::FFieldClass::Id + 0x10) && Off::FFieldClass::SuperClass == (Off::FFieldClass::CastFlags + 0x08))
+		constexpr int32 FFieldClassReorderedCastFlagsOffsetFromClassFlags = static_cast<int32>(Align(sizeof(EClassFlags), alignof(EFieldClassID)) + sizeof(EFieldClassID));
+		if (Off::FFieldClass::CastFlags == (Off::FFieldClass::Id + FFieldClassReorderedCastFlagsOffsetFromClassFlags) && Off::FFieldClass::SuperClass == (Off::FFieldClass::CastFlags + sizeof(EClassCastFlags)))
 		{
 			Off::FFieldClass::ClassFlags = Off::FFieldClass::Id;
-			Off::FFieldClass::Id = Off::FFieldClass::CastFlags - 0x08;
+			Off::FFieldClass::Id = Off::FFieldClass::CastFlags - sizeof(EFieldClassID);
 		}
 
 		std::cerr << std::format("Off::FFieldClass::CastFlags: 0x{:X}\n\n", Off::FFieldClass::CastFlags);
